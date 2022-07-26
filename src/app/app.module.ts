71cbb30b17057accd01ee7d7870698b5e1eaa0ab
Aditya Gurnani <adityagurnani@gmail.com>
2022-07-26 17:07:46 +0530

    forms

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -21,6 +21,10 @@ import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
 import { ContactComponent } from './contact/contact.component';
 import { LoginComponent } from './login/login.component';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatCheckboxModule } from '@angular/material/checkbox';
+import { FormsModule } from '@angular/forms'; 
 import 'hammerjs';
 
 @NgModule({
@@ -45,6 +49,10 @@ import 'hammerjs';
     MatDialogModule,
     MatCardModule,
     MatButtonModule,
+    MatFormFieldModule, 
+    MatInputModule,
+    MatCheckboxModule,
+    FormsModule,
     FlexLayoutModule
   ],
   providers: [
